c13d05b2919464b3c2cc73b21d71779b831d836c
Łukasz Lis <92014825+llis1991@users.noreply.github.com>
2022-01-24 21:34:05 +0100

    code optimization

--- lotto.py
+++ lotto.py
@@ -1,35 +1,5 @@
 
 import random
-'''
-def losowanieLotto(numbersOfBalls, BallsToDraw):
-    lottoList = []
-    for i in range(0, BallsToDraw):
-        drawNumber = random.randint(1, numbersOfBalls)
-        if drawNumber not in lottoList:
-            lottoList.append(drawNumber)
-        elif drawNumber in lottoList:
-            
-    return lottoList
-
-print(losowanieLotto(49, 6))
-
-
-for i in range(0, a==5):
-        drawNumber = random.randint(1, 10)
-        if drawNumber < 5:
-            print(drawNumber)
-        elif drawNumber > 5:
-
-
-liczba = 0
-while liczba < 5:
-    drawNumber = random.randint(0, 10)
-    if drawNumber < 5:
-        print(drawNumber)
-        liczba += 1
-    elif drawNumber > 5:
-        liczba -= 1
-'''
 
 def losowanieLotto(numbersOfBalls, ballsToDraw):
     lottoList = []
